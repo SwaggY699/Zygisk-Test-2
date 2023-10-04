97b58ba4b4ad698d0cb63067d8995d341333d73d
SwaggY <74751337+SwaggY699@users.noreply.github.com>
2023-10-05 00:13:59 +0300

    Update hook.cpp

--- module/src/main/cpp/hook.cpp
+++ module/src/main/cpp/hook.cpp
@@ -24,9 +24,6 @@
 #include "il2cpp-class.h"
 #include "KittyMemory/MemoryPatch.h"
 
-inline std::map < std::string, void*> _methods;
-inline std::map < std::string, size_t > _fields;
-
 #define GamePackageName "com.ngame.allstar.eu"
 
 int isGame(JNIEnv *env, jstring appDataDir) {
@@ -95,127 +92,6 @@ void  il2cpp_api_init(void *handle) {
     il2cpp_thread_attach(domain);
 }
 ////////////////////////////////////////////////////////////////////////////////////
-void *Il2CppGetImageByName(const char *image);
-void *Il2CppGetClassType(const char *image, const char *namespaze, const char *clazz);
-void *Il2CppGetMethodOffset(const char *image, const char *namespaze, const char *clazz, const char *name, int argsCount = 0);
-void *Il2CppGetMethodOffset(const char *image, const char *namespaze, const char *clazz, const char *name, char **args, int argsCount);
-////////////////////////////////////////////////////////////////////////////////////
-void *Il2CppGetImageByName(const char *image) {
-    size_t size;
-    void **assemblies = il2cpp_domain_get_assemblies(il2cpp_domain_get(), &size);
-    for (int i = 0; i < size; ++i) {
-        void *img = (void *)il2cpp_assembly_get_image(assemblies[i]);
-        const char *img_name = il2cpp_image_get_name(img);
-        if (strcmp(img_name, image) == 0) {
-            return img;
-        }
-    }
-    return 0;
-}
-
-void *Il2CppGetClassType(const char *image, const char *namespaze, const char *clazz) {
-    static std::map<std::string, void *> cache;
-    std::string s = image;
-    s += namespaze;
-    s += clazz;
-    if (cache.count(s) > 0) return cache[s];
-    void *img = Il2CppGetImageByName(image);
-    if (!img) {
-        LOG(ANDROID_LOG_ERROR, g_LogTag, "Can't find image %s!", image);
-        return 0;
-    }
-    void *klass = il2cpp_class_from_name(img, namespaze, clazz);
-    if (!klass) {
-        LOG(ANDROID_LOG_ERROR, g_LogTag, "Can't find class %s!", clazz);
-        return 0;
-    }
-    cache[s] = klass;
-    return klass;
-}
-
-void *Il2CppGetMethodOffset(const char *image, const char *namespaze, const char *clazz, const char *name, int argsCount) {
-    void *img = Il2CppGetImageByName(image);
-    if (!img) {
-        LOG(ANDROID_LOG_ERROR, g_LogTag, "Can't find image %s!", image);
-        return 0;
-    }
-    void *klass = Il2CppGetClassType(image, namespaze, clazz);
-    if (!klass) {
-        LOG(ANDROID_LOG_ERROR, g_LogTag, "Can't find method %s!", name);
-        return 0;
-    }
-    void **method = (void **)il2cpp_class_get_method_from_name(klass, name, argsCount);
-    if (!method) {
-        LOG(ANDROID_LOG_ERROR, g_LogTag, "Can't find method %s in class %s!", name, clazz);
-        return 0;
-    }
-    LOG(ANDROID_LOG_DEBUG, g_LogTag, "%s - [%s] %s::%s: %p", image, namespaze, clazz, name, *method);
-    return *method;
-}
-
-void *Il2CppGetMethodOffset(const char *image, const char *namespaze, const char *clazz, const char *name, char **args, int argsCount) {
-    void *img = Il2CppGetImageByName(image);
-    if (!img) {
-        LOG(ANDROID_LOG_ERROR, g_LogTag, "Can't find image %s!", image);
-        return 0;
-    }
-    void *klass = Il2CppGetClassType(image, namespaze, clazz);
-    if (!klass) {
-        LOG(ANDROID_LOG_ERROR, g_LogTag, "Can't find class %s for method %s!", clazz, name);
-        return 0;
-    }
-    void *iter = 0;
-    int score = 0;
-    void **method = (void **)il2cpp_class_get_methods(klass, &iter);
-
-    while (method) {
-        const char *fname = il2cpp_method_get_name(method);
-        if (strcmp(fname, name) == 0) {
-            for (int i = 0; i < argsCount; i++) {
-                void *arg = il2cpp_method_get_param(method, i);
-                if (arg) {
-                    const char *tname = il2cpp_type_get_name(arg);
-                    if (strcmp(tname, args[i]) == 0) {
-                        score++;
-                    } else {
-                        LOG(ANDROID_LOG_INFO, g_LogTag, "Argument at index %d didn't matched requested argument!\n\tRequested: %s\n\tActual: %s\nnSkipping function...", i, args[i], tname);
-                        score = 0;
-                        goto skip;
-                    }
-                }
-            }
-        }
-    skip:
-        if (score == argsCount) {
-            LOG(ANDROID_LOG_DEBUG, g_LogTag, "%s - [%s] %s::%s: %p", image, namespaze, clazz, name, *method);
-            return *method;
-        }
-        method = (void **)il2cpp_class_get_methods(klass, &iter);
-    }
-    LOG(ANDROID_LOG_ERROR, g_LogTag, "Cannot find function %s in class %s!", name, clazz);
-    return 0;
-}
-////////////////////////////////////////////////////////////////////////////////////
-il2cpp_assembly_get_image = (const void *(*)(const void *))xdl_sym(handle, "il2cpp_assembly_get_image", 0);
-    il2cpp_domain_get = (void *(*)())xdl_sym(handle, "il2cpp_domain_get", 0);
-    il2cpp_domain_get_assemblies = (void **(*)(const void *, size_t *))xdl_sym(handle, "il2cpp_domain_get_assemblies", 0);
-    il2cpp_image_get_name = (const char *(*)(void *))xdl_sym(handle, "il2cpp_image_get_name", 0);
-    il2cpp_class_from_name = (void *(*)(const void *, const char *, const char *))xdl_sym(handle, "il2cpp_class_from_name", 0);
-    il2cpp_class_get_field_from_name = (void *(*)(void *, const char *))xdl_sym(handle, "il2cpp_class_get_field_from_name", 0);
-    il2cpp_class_get_method_from_name = (void *(*)(void *, const char *, int))xdl_sym(handle, "il2cpp_class_get_method_from_name", 0);
-    il2cpp_field_get_offset = (size_t(*)(void *))xdl_sym(handle, "il2cpp_field_get_offset", 0);
-    il2cpp_field_static_get_value = (void (*)(void *, void *))xdl_sym(handle, "il2cpp_field_static_get_value", 0);
-    il2cpp_field_static_set_value = (void (*)(void *, void *))xdl_sym(handle, "il2cpp_field_static_set_value", 0);
-    il2cpp_array_new = (void *(*)(void *, size_t))xdl_sym(handle, "il2cpp_array_new", 0);
-    il2cpp_string_chars = (uint16_t * (*)(void *)) xdl_sym(handle, "il2cpp_string_chars", 0);
-    il2cpp_string_new = (Il2CppString * (*)(const char *)) xdl_sym(handle, "il2cpp_string_new", 0);
-    il2cpp_string_new_utf16 = (Il2CppString * (*)(const wchar_t *, int32_t)) xdl_sym(handle, "il2cpp_string_new", 0);
-    il2cpp_type_get_name = (char *(*)(void *))xdl_sym(handle, "il2cpp_type_get_name", 0);
-    il2cpp_method_get_param = (void *(*)(void *, uint32_t))xdl_sym(handle, "il2cpp_method_get_param", 0);
-    il2cpp_class_get_methods = (void *(*)(void *, void **))xdl_sym(handle, "il2cpp_class_get_methods", 0);
-    il2cpp_method_get_name = (const char *(*)(void *))xdl_sym(handle, "il2cpp_method_get_name", 0);
-    il2cpp_object_new = (void *(*)(void *))xdl_sym(handle, "il2cpp_object_new", 0);
-///////////////////////////////////////////////////////////////////////////////////
 const char* nop = "1F2003D5";
 const char* fal = "000080D2C0035FD6";
 const char* tru = "200080D2C0035FD6";
@@ -313,8 +189,7 @@ void *hack_thread(void *arg) {
     
     Patches();
     
-    _methods["Screen::SetResolution"] = Il2CppGetMethodOffset("UnityEngine.CoreModule.dll", "UnityEngine", "Screen", "SetResolution", 3);
-    DobbyHook((void *)&_methods["Screen::SetResolution"], (void *)SetResolutionn, (void **)&_SetResolutionn);
+    DobbyHook((void *) il2cpp_base + 0x44A3F00, (void *)SetResolutionn, (void **)&_SetResolutionn);
     
     return nullptr;
 }
